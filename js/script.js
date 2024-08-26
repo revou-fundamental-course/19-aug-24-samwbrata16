54e6105629e289d656175c046728acf6dfcd531d
samwbrata16 <69029403+samwbrata16@users.noreply.github.com>
2024-08-26 23:19:51 +0700

    update mini project 26-08-24

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 // Ini JavaScript
+
+function validateForm() {
+	const name = document.forms["message-form"]["name"].value;
+	const birthDate = document.forms["message-form"]["birth-date"].value;
+	const gender = document.forms["message-form"]["gender"].value;
+	const message = document.forms["message-form"]["message"].value;
+		
+	if (name == "" || birthDate == "" || gender == "" || message == "") {
+		alert("There can be no empty fields.");
+		return false;
+	}
+		
+	setSenderUI(name, birthDate, gender, message);
+		
+	return false;
+}
+
+function setSenderUI(name, birthDate, gender, message) {
+	document.getElementById("sender-name").innerHTML = name;
+	document.getElementById("sender-birth-date").innerHTML = birthDate;
+	document.getElementById("sender-gender").innerHTML = gender;
+	document.getElementById("sender-message").innerHTML = message;
+} 	
