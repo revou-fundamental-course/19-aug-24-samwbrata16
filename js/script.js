a375b1387b3cdeb09dbeaa98fe76ef921a273459
samwbrata16 <69029403+samwbrata16@users.noreply.github.com>
2024-08-27 23:37:02 +0700

    update mini project 27-08-24

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
 // Ini JavaScript
 
+var slideIndex = 1;
+	showDivs(slideIndex);
+	
+function plusDivs(n){
+	showDivs((slideIndex += n));
+}	
+	
+function showDivs(n) {
+	var i;
+	var imgList = document.getElementsByClassName("img-slideshow");
+	if (n > imgList.length) slideIndex = 1;
+	else if (n < 1) slideIndex = imgList.length;
+	
+	for(i = 0; i < imgList.length; i++) {
+		imgList[i].style.display = "none";
+}
+	imgList[slideIndex - 1].style.display = "block";
+		
+}
+
+setInterval(() => {
+	plusDivs(1);
+}, 3000)	
+
 function validateForm() {
 	const name = document.forms["message-form"]["name"].value;
 	const birthDate = document.forms["message-form"]["birth-date"].value;
